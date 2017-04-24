f199e3d2f32cfdb2f062d2ae259cc500452b949e
Michael Mitchell <mikeybags@gmail.com>
2017-04-24 14:17:50 -0500

    navbar and sass conversion

--- django_and_react/assets/js/react/containers/Header.js
+++ django_and_react/assets/js/react/containers/Header.js
@@ -5,6 +5,7 @@ import {createSession} from '../actions/create_session'
 import {getSession} from '../actions/get_session'
 import {Link} from 'react-router'
 import { getCookie, expireCookie } from 'redux-cookie';
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown, FormGroup, FormControl, Button, NavLink } from 'react-bootstrap'
 
 
 class Header extends Component {
@@ -30,22 +31,53 @@ class Header extends Component {
   render(){
     const session = this.props.session
     if (!session.hasOwnProperty("id")){
-      return (
-        <div>
-          <h3>Welcome, please
-            <Link to='/'> register</Link> or
-            <Link to='login'> login</Link>
-          </h3>
-        </div>
-      )
+      const navbarInstance = (
+        <Navbar className="navbar navbar-inverse navbar-toggleable-md" id="navbar" fixedTop inverse collapseOnSelect>
+          <Navbar.Header>
+          <a className="navbar-brand" href="#">Strive</a>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <li className="nav-buttons" role="presentation"><a href='#/'>Register</a></li>
+              <li className="nav-buttons" role="presentation"><a href='#/login'>Log In</a></li>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      );
+
+      return (navbarInstance);
     }
     else{
-      return (
-        <div>
-          <h3>Hello {session.name}</h3>
-          <button className="btn btn-warning" onClick={this.onLogout.bind(this)}>Logout</button>
-        </div>
-      )
+      const navbarInstance = (
+        <Navbar className="navbar navbar-inverse navbar-toggleable-md" id="navbar" fixedTop inverse collapseOnSelect>
+          <Navbar.Header>
+          <a className="navbar-brand" href="#">Strive</a>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup id="nav-form" className="form-inline">
+              <FormControl id="nav-search" className="form-control mr-sm-2" type="text" placeholder="Search for Friends" />
+              <Button id="nav-submit" className="form-control mr-sm-2" type="submit">Submit</Button>
+            </FormGroup>
+          </Navbar.Form>
+            <Nav pullRight>
+              <NavDropdown className="name-dropdown" noCaret eventKey={3} title={session.name} id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Stuff</MenuItem>
+                <MenuItem eventKey={3.2}>More Stuff</MenuItem>
+                <MenuItem eventKey={3.3}>Even More Stuff else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated Stuff</MenuItem>
+              </NavDropdown>
+              <NavItem className="nav-buttons">Store</NavItem>
+              <NavItem className="nav-buttons" onClick={this.onLogout.bind(this)}>Logout</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      );
+
+      return (navbarInstance);
     }
   }
 }
