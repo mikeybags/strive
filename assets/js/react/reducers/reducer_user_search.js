cb5310924b61e3f1d621609390ec06db3d6c2f0b
Michael Mitchell <mikeybags@gmail.com>
2017-04-26 14:40:08 -0500

    removed logs

--- assets/js/react/reducers/reducer_user_search.js
+++ assets/js/react/reducers/reducer_user_search.js
@@ -3,9 +3,7 @@ import { FRIEND_SEARCH } from '../actions/types'
 export default function(state = [], action) {
   switch(action.type) {
     case FRIEND_SEARCH:
-      console.log("reducer!")
       const users = action.payload.data.users
-      console.log(users)
       return users;
     default:
       return state;
