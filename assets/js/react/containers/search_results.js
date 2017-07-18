be13870fe151d0f9a8a827d494f4b3c9ba35c2f2
Michael Mitchell <mikeybags@gmail.com>
2017-07-17 20:25:09 -0500

    cleaned up logic on friend search/add and improved styling

--- assets/js/react/containers/search_results.js
+++ assets/js/react/containers/search_results.js
@@ -5,10 +5,18 @@ import { requestFriend } from '../actions/request_friend'
 class SearchResults extends Component {
   constructor(props){
     super(props);
+    console.log(props);
     this.state = {
-      term: ''
+      message: '',
+      users: props.data.users
     }
   }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('next props', nextProps);
+    this.setState({message: '', users: nextProps.data.users});
+  }
+
   renderUsers(){
     const users = this.props.data.users;
     const term = this.props.data.term;
@@ -30,37 +38,45 @@ class SearchResults extends Component {
               </div>
             </div>
           </div>
-        )
+        );
       });
     } else {
       return (
         <div>Enter a search term to search for a friend.</div>
-      )
+      );
     }
   }
 
   handleSentRequest(id) {
     this.props.requestFriend(id);
-    for(var i = 0; i < this.props.data.users.length; i++) {
-      if(this.props.data.users[i].id == id) {
-        this.props.data.users.splice(i, 1);
-        this.setState({search: this.props.data})
+    for(var i = 0; i < this.state.users.length; i++) {
+      if(this.state.users[i].id == id) {
+        let message = `Your friend request to ${this.state.users[i].username} has been sent.`;
+        this.setState({message, data: this.state.users.splice(i, 1) });
       }
     }
   }
 
   render(){
+    console.log('props in render', this.props);
+    console.log('state in render', this.state);
     return (
       <div>
-        <h5>Usernames matching "{this.props.data.term}":</h5>
-          {this.renderUsers()}
+        {this.state.users ? <h5>Usernames matching "{this.state.term}" :</h5> : '' }
+        {this.state.users && this.state.users.length === 0 && !this.state.message ? <p>No users found matching "{this.state.term}". Enter another term to search again.</p> : ''}
+        {this.state.users && this.state.users.length === 0 && this.state.message ? <p>No more users found matching "{this.state.term}". Enter another term to search again.</p> : ''}
+        {this.state.message ? <div className="card text-success request-sent col-xs-12">{this.state.message}</div> : null}
+        {this.renderUsers()}
       </div>
     )
   }
 }
 
 function mapStateToProps(state){
-  return { data: state.search }
+  return { 
+    data: state.search
+   }
 }
 
 export default connect(mapStateToProps, {requestFriend})(SearchResults);
+
